b8f344eda5d812e27c86e3ab5ded30fdfaad4bb2
Per Lindgren <per.lindgren@ltu.se>
2024-08-27 16:45:26 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(
+        global = true, // name = arg form, neat for one-arg methods
+        required // name(arg1, arg2, ...) form.
+    )]
+    /// Optional path to operate on
+    path: Option<PathBuf>,
+
+    /// Compile an example (will look in the examples folder)
+    #[arg(long)]
+    example: bool,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    // Path to file to compile
+    if let Some(path) = cli.path.as_deref() {
+        println!("File path: {:?}", path);
+    } else {
+        println!("please")
+    }
+
+    //
+    println!("Is example: {}", cli.example);
+
+    if let Some(config_path) = cli.config.as_deref() {
+        println!("Value for config: {}", config_path.display());
+    }
+
+    // You can see how many times a particular flag or argument occurred
+    // Note, only flags can have multiple occurrences
+    match cli.debug {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Some(Commands::Test { list }) => {
+            if *list {
+                println!("Printing testing lists...");
+            } else {
+                println!("Not printing testing lists...");
+            }
+        }
+        None => {}
+    }
+
+    // Continued program logic goes here...
+}
